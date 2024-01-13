723eb2ea38bf453379013dc91b9b378bef84bde2
ilyapatis24 <ilyapatis24@mail.ru>
2024-01-13 22:50:04 +0300

    first mission complete

---
+++ 01/main.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+using namespace std;
+typedef unsigned long long ull;
+
+ull fibonacce(int index) {
+    if (index == 0) return 0;
+    if (index == 1 || index == 2) return 1;
+
+    return fibonacce(index - 1) + fibonacce(index - 2);
+}
+
+int main(int argc, char** argv) {
+    system("chcp 1251");
+    int number = 0;
+    cout << "Введите число: ";
+    cin >> number;
+    cout << "Числа Фибоначчи: ";
+    for (int i = 0; i < number; ++i) {
+        cout << fibonacce(i) << " ";
+    }
+    return 0;
+}
